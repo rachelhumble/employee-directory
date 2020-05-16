2cf1e1c16bc60889b69460b833530a6fd63065ee
Rachel Humble <rachelhumble@me.com>
2020-05-15 20:26:09 -0400

    Deploy updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # employee-directory
  An employee directory REACT application for hw19
+
+### Description
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Header from "./components/header";
 import NavTabs from "./components/navtabs";
 import Main from "./components/pages/main";
@@ -10,13 +11,13 @@ function App() {
   return (
     <div className="App">
       <Header />
-        <Router>
+        <HashRouter>
           <div className="body">
             <NavTabs />
             <Route exact path="/" component={Main} />
             <Route exact path="/department" component={Department} />
           </div>
-        </Router>
+        </HashRouter>
     </div>
   );
 }
